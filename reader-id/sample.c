bbfcd1cac204e2491529f11a5f802e3a1180c1e7
Mitchell Bohn <mwbohn@mavs.coloradomesa.edu>
2019-07-18 13:02:42 -0600

    Removed helper files

--- reader-id/sample.c
+++ reader-id/sample.c
@@ -1,8 +1,10 @@
-#include "helpers.h"
 #include "menu.h"
 #include "selection.h"
 
 #include <dpfpdd.h>
+#include <dpfj.h>
+#include <dpfj_compression.h>
+#include <dpfj_quality.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,6 +12,7 @@
 #include <string.h>
 #include <signal.h>
 #include <locale.h>
+#include <sys/time.h>
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -25,60 +28,54 @@ int main(int argc, char** argv){
 
 	//initialize capture library
 	int result = dpfpdd_init();
-	if(DPFPDD_SUCCESS != result) print_error("dpfpdd_init()", result);
-	else{
-		DPFPDD_DEV hReader = NULL; //handle of the selected reader
-		char szReader[MAX_DEVICE_NAME_LENGTH]; //name of the selected reader
+	DPFPDD_DEV hReader = NULL; //handle of the selected reader
+	char szReader[MAX_DEVICE_NAME_LENGTH]; //name of the selected reader
 
-		menu_t* pMenu = NULL;
-		int res = Menu_Create("ReaderID", MENU_TYPE_EXIT, &pMenu);
-		if(0 == res) res = Menu_AddItem(pMenu, 101, "Select new reader (not selected)");
-		if(0 == res){
-			//main menu loop
-			int bStop = 0;
-			while(!bStop){
-				int nChoice = 0;
-				Menu_DoModal(pMenu, &nChoice);
+	menu_t* pMenu = NULL;
+	int res = Menu_Create("ReaderID", MENU_TYPE_EXIT, &pMenu);
+	if(0 == res) res = Menu_AddItem(pMenu, 101, "Select new reader (not selected)");
+	if(0 == res){
+		//main menu loop
+		int bStop = 0;
+		while(!bStop){
+			int nChoice = 0;
+			Menu_DoModal(pMenu, &nChoice);
 
-				switch(nChoice){
-					case 101: //select reader
-						//close reader if opened
-						if(NULL != hReader){
-							result = dpfpdd_close(hReader);
-							if(DPFPDD_SUCCESS != result) print_error("dpfpdd_close()", result);
-							hReader = NULL;
-						}
-						//open new reader
-						hReader = SelectAndOpenReader(szReader, sizeof(szReader));
-						if(NULL != hReader){
-							char szItem[MAX_DEVICE_NAME_LENGTH + 20];
-							snprintf(szItem, sizeof(szItem), "Select new reader (selected: %s)", szReader);
-							Menu_AddItem(pMenu, 101, szItem);
-						}
-						else{
-							Menu_AddItem(pMenu, 101, "Select new reader (not selected)");
-						}
-						break;
-					case -2: //exit
-						bStop = 1;
-						break;
-				}
+			switch(nChoice){
+				case 101: //select reader
+					//close reader if opened
+					if(NULL != hReader){
+						result = dpfpdd_close(hReader);
+						hReader = NULL;
+					}
+					//open new reader
+					hReader = SelectAndOpenReader(szReader, sizeof(szReader));
+					if(NULL != hReader){
+						char szItem[MAX_DEVICE_NAME_LENGTH + 20];
+						snprintf(szItem, sizeof(szItem), "Select new reader (selected: %s)", szReader);
+						Menu_AddItem(pMenu, 101, szItem);
+					}
+					else{
+						Menu_AddItem(pMenu, 101, "Select new reader (not selected)");
+					}
+					break;
+				case -2: //exit
+					bStop = 1;
+					break;
 			}
-
-			Menu_Destroy(pMenu);
-		}
-		else print_error("Menu_Create() or Menu_AddItem()", res);
-
-		//close reader
-		if(NULL != hReader){
-			result = dpfpdd_close(hReader);
-			if(DPFPDD_SUCCESS != result) print_error("dpfpdd_close()", result);
-			hReader = NULL;
 		}
 
-		//relese capture library
-		dpfpdd_exit();
+		Menu_Destroy(pMenu);
 	}
+
+	//close reader
+	if(NULL != hReader){
+		result = dpfpdd_close(hReader);
+		hReader = NULL;
+	}
+
+	//relese capture library
+	dpfpdd_exit();
 
 	return 0;
 }
--- reader-id/selection.h
+++ reader-id/selection.h
@@ -1,9 +1,3 @@
-/* 
- * Copyright (C) 2011, Digital Persona, Inc.
- *
- * This file is a part of sample code for the UareU SDK 2.x.
- */
-
 #pragma once
 
 #include <dpfpdd.h>
